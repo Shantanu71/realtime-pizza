f2471f24f4e598f7bf12403d53c1b1c1050ec595
shantanu kumar <shantanu.kumar@freecharge.com>
2021-07-04 17:14:15 +0530

    talwind css

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+
+
+const app = express()
+const PORT = process.env.PORT || 3300
+
+app.get('/', (req, res) => res.render('home'))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+
+
+// Assets
+app.use(express.static('public'))
+
+// set Template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
